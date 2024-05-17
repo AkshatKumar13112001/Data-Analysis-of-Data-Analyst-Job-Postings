030c9921c183df00c6c829670febeef44d709df0
AkshatKumar13112001 <118676207+AkshatKumar13112001@users.noreply.github.com>
2024-05-17 08:44:24 +0530

    Update sql_analysis.sql

--- sql_analysis.sql
+++ sql_analysis.sql
@@ -1,11 +1,6 @@
 /*
 	Data Analyst Job Postings
-	SQL Author: Jaime M. Shaker
-	Dataset Creator: Luke Barousse (https://www.linkedin.com/in/luke-b/) (https://www.youtube.com/@LukeBarousse)
 	Dataset Location: https://www.kaggle.com/datasets/lukebarousse/data-analyst-job-postings-google-search
-	Email: jaime.m.shaker@gmail.com or jaime@shaker.dev
-	Website: https://www.shaker.dev
-	LinkedIn: https://www.linkedin.com/in/jaime-shaker/
 	
 	File Name: sql_analysis.sql
 	Description:  The aim of this analysis is to gain insight into the skills that are frequently mentioned in the 
